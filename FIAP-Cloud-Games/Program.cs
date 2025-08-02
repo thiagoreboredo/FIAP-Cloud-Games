04560db11c258e1e2e248cc24c0f81da5b0d3c2b
Thiago Reboredo <thiagoreboredo@gmail.com>
2025-08-02 15:02:35 -0300

    fix(api): Habilita Swagger em ambiente de Produção
    - Remove a condição 'IsDevelopment()' da configuração do Swagger no Program.cs.
    - Esta alteração garante que a documentação da API (Swagger UI) fique acessível no ambiente de nuvem para fins de teste e demonstração.

--- FIAP-Cloud-Games/Program.cs
+++ FIAP-Cloud-Games/Program.cs
@@ -104,12 +104,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
